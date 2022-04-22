146f8237039ad0f8b2cd138dbf1d8faa6bfe44b0
rihongo <rihongo@gmail.com>
2022-04-22 16:20:42 +0900

    first commit

--- auth-api/src/main/java/com/bithumbsystems/auth/api/exception/ApiException.java
+++ auth-api/src/main/java/com/bithumbsystems/auth/api/exception/ApiException.java
@@ -2,11 +2,6 @@ package com.bithumbsystems.auth.api.exception;
 
 import lombok.Getter;
 
-/**
- * ApiException class
- *
- * @author Erik Amaru Ortiz
- */
 public class ApiException extends RuntimeException {
     @Getter
     protected String errorCode;
