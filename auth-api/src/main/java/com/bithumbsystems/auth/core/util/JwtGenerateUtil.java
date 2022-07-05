7291ed95fff10ead995bd68908ce090f5e8dc6fd
rihongo-at-807380035085 <3zaHkd3zJ87T1icEEOSTNLAHlAdGniO9sZT2LJlmeo4=>
2022-07-05 16:33:42 +0900

    token 생성 수정 - generateOtp, generate expiration 통일

--- auth-api/src/main/java/com/bithumbsystems/auth/core/util/JwtGenerateUtil.java
+++ auth-api/src/main/java/com/bithumbsystems/auth/core/util/JwtGenerateUtil.java
@@ -8,22 +8,13 @@ import io.jsonwebtoken.security.Keys;
 import java.util.Date;
 import java.util.UUID;
 
-public final class JwtGenerateUtil {
+public class JwtGenerateUtil {
 
     public static TokenInfo generate(GenerateTokenInfo generateTokenInfo) {
-        var expirationTimeInMilliseconds = Long.parseLong(generateTokenInfo.getExpiration()) * 1000;
-        var expirationDate = new Date(System.currentTimeMillis() + expirationTimeInMilliseconds);
+        Date expirationDate = getExpirationDate(generateTokenInfo);
 
         var createdDate = new Date();
-        var token = Jwts.builder()
-                .setClaims(generateTokenInfo.getClaims())
-                .setIssuer(generateTokenInfo.getIssuer())
-                .setSubject(generateTokenInfo.getSubject())
-                .setIssuedAt(createdDate)
-                .setId(UUID.randomUUID().toString())
-                .setExpiration(expirationDate)
-                .signWith(Keys.hmacShaKeyFor(generateTokenInfo.getSecret().getBytes()))
-                .compact();
+        var token = makeToken(generateTokenInfo, expirationDate, createdDate);
 
         // refreshToken
         var refreshToken = Jwts.builder()
@@ -45,25 +36,34 @@ public final class JwtGenerateUtil {
     }
 
     public static TokenOtpInfo generateOtp(GenerateTokenInfo generateTokenInfo) {
-        var expirationTimeInMilliseconds = Long.parseLong(generateTokenInfo.getExpiration()) * 1000;
-        var expirationDate = new Date(new Date().getTime() + expirationTimeInMilliseconds);
+        Date expirationDate = getExpirationDate(generateTokenInfo);
 
         var createdDate = new Date();
-        var token = Jwts.builder()
-                .setClaims(generateTokenInfo.getClaims())
-                .setIssuer(generateTokenInfo.getIssuer())
-                .setSubject(generateTokenInfo.getSubject())
-                .setIssuedAt(createdDate)
-                .setId(UUID.randomUUID().toString())
-                .setExpiration(expirationDate)
-                .signWith(Keys.hmacShaKeyFor(generateTokenInfo.getSecret().getBytes()))
-                .compact();
+        var token = makeToken(generateTokenInfo, expirationDate, createdDate);
 
         return TokenOtpInfo.builder()
                 .token(token)
                 .issuedAt(createdDate)
                 .expiresAt(expirationDate)
                 .build();
+    }
+
+    private static String makeToken(GenerateTokenInfo generateTokenInfo, Date expirationDate,
+        Date createdDate) {
+        return Jwts.builder()
+            .setClaims(generateTokenInfo.getClaims())
+            .setIssuer(generateTokenInfo.getIssuer())
+            .setSubject(generateTokenInfo.getSubject())
+            .setIssuedAt(createdDate)
+            .setId(UUID.randomUUID().toString())
+            .setExpiration(expirationDate)
+            .signWith(Keys.hmacShaKeyFor(generateTokenInfo.getSecret().getBytes()))
+            .compact();
+    }
+
+    private static Date getExpirationDate(GenerateTokenInfo generateTokenInfo) {
+        var expirationTimeInMilliseconds = Long.parseLong(generateTokenInfo.getExpiration()) * 1000;
+        return new Date(System.currentTimeMillis() + expirationTimeInMilliseconds);
     }
 }
 
